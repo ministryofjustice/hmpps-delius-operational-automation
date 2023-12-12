a2aaa193f483d0bfd5f38405104af967c6bc3d83
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2023-12-12 17:50:01 +0000

    Do not provide Perfstat password on the command line

--- playbooks/oracle_statspack/oracle_statspack/files/install_statspack.sh
+++ playbooks/oracle_statspack/oracle_statspack/files/install_statspack.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# We use a random password for the PERFSTAT user as we do not need to login to
+# this account, so the password does not need to be stored, and the account is
+# locked after creation.
 
 . ~/.bash_profile
 
@@ -11,7 +15,7 @@ SET PAGES 0
 SET FEEDBACK OFF
 SET HEADING OFF
 WHENEVER SQLERROR EXIT FAILURE
-DEFINE perfstat_password=$1
+DEFINE perfstat_password=p#$(openssl rand -base64 15)
 DEFINE default_tablespace=STATSPACK_DATA
 DEFINE temporary_tablespace=$2
 @?/rdbms/admin/spcreate
