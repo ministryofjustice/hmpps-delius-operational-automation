ee050e60a81117cd2d3d2c8d1d0075e64eec01d7
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2023-12-12 18:08:38 +0000

    Fix positional parameters

--- playbooks/oracle_statspack/oracle_statspack/files/install_statspack.sh
+++ playbooks/oracle_statspack/oracle_statspack/files/install_statspack.sh
@@ -3,6 +3,8 @@
 # We use a random password for the PERFSTAT user as we do not need to login to
 # this account, so the password does not need to be stored, and the account is
 # locked after creation.
+#
+# Name of the temporary tablespace to be used is supplied as a parameter
 
 . ~/.bash_profile
 
@@ -17,7 +19,7 @@ SET HEADING OFF
 WHENEVER SQLERROR EXIT FAILURE
 DEFINE perfstat_password=p#$(openssl rand -base64 15)
 DEFINE default_tablespace=STATSPACK_DATA
-DEFINE temporary_tablespace=$2
+DEFINE temporary_tablespace=$1
 @?/rdbms/admin/spcreate
 EXIT
 EOF
