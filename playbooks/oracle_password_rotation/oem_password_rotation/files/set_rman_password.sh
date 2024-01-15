b42190bfbdfc9e14b3454343d39e3b23e44027d1
ranbeersingh1 <ranbeersinghtradedata@hotmail.com>
2024-01-15 15:07:10 +0000

    Replace rman test user with rcvcatowner schema

--- playbooks/oracle_password_rotation/oem_password_rotation/files/set_rman_password.sh
+++ playbooks/oracle_password_rotation/oem_password_rotation/files/set_rman_password.sh
@@ -8,10 +8,10 @@ ORACLE_SID=${DB_NAME}
 . oraenv
 ORAENV_ASK=YES
 
-RMAN_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${SECRET_ID} --region eu-west-2 --query SecretString --output text| jq -r .rman)
+RMAN_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${SECRET_ID} --region eu-west-2 --query SecretString --output text| jq -r .rcvcatowner)
 
 sqlplus /nolog <<EOSQL
 connect / as sysdba
-alter user rman identified by ${RMAN_PASSWORD};
+alter user rcvcatowner identified by ${RMAN_PASSWORD};
 exit;
 EOSQL
