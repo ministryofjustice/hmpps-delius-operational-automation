f830f10a12b68aa65063f07a1a3820788be46355
ranbeersingh1 <ranbeersinghtradedata@hotmail.com>
2024-02-23 12:27:53 +0000

    Changes to handle catalog

--- playbooks/oracle_restore_points/files/flashback_database_rman.sh
+++ playbooks/oracle_restore_points/files/flashback_database_rman.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+get_rman_password () {
+  ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+  ROLE_ARN="arn:aws:iam::${ACCOUNT_ID}:role/${ASSUME_ROLE_NAME}"
+  SESSION="catalog-ansible"
+  CREDS=$(aws sts assume-role --role-arn "${ROLE_ARN}" --role-session-name "${SESSION}"  --output text --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]")
+  export AWS_ACCESS_KEY_ID=$(echo "${CREDS}" | tail -1 | cut -f1)
+  export AWS_SECRET_ACCESS_KEY=$(echo "${CREDS}" | tail -1 | cut -f2)
+  export AWS_SESSION_TOKEN=$(echo "${CREDS}" | tail -1 | cut -f3)
+  SECRET_ARN="arn:aws:secretsmanager:eu-west-2:${SECRET_ACCOUNT_ID}:secret:${SECRET}"
+  RMANPASS=$(aws secretsmanager get-secret-value --secret-id "${SECRET_ARN}" --query SecretString --output text | jq -r .rcvcatowner)
+}
+
 . ~/.bash_profile
 
-[[ ! -z "$CATALOG_CONNECTION" ]] && CONNECT_CATALOG="connect catalog ${CATALOG_CONNECTION}"
+if [[ ! -z "$CATALOG" ]] 
+then
+   get_rman_password
+   CONNECT_CATALOG="connect catalog rcvcatowner/${RMANPASS}@${CATALOG}"
+fi
 
 rman target / <<EORMAN
 
