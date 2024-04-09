17568232e95ff57589fd85189eba68a2bf657911
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-04-09 17:57:55 +0100

    Added commentary

--- playbooks/oracle_backup/rman_backup.sh
+++ playbooks/oracle_backup/rman_backup.sh
@@ -88,17 +88,18 @@ function generate_jwt()
 {
 # Get a JSON Web Token to authenicate against the HMPPS Bot.
 # The HMPPS bot can provide exchange this for a GitHub Token for action GitHub workflows.
-
 BOT_APP_ID=$(aws ssm get-parameter --name "/github/hmpps_bot_app_id" --query "Parameter.Value" --with-decryption --output text)
 BOT_PRIVATE_KEY=$(aws ssm get-parameter --name "/github/hmpps_bot_priv_key" --query "Parameter.Value" --with-decryption --output text)
 
-# Define expiry time for JWT
+# Define expiry time for JWT - we will be using it immediately so just use a 10 minute expiry time.
 NOW=$(date +%s)
-INITIAL=$((${NOW} - 60)) # Issues 60 seconds in the past
+INITIAL=$((${NOW} - 60)) # Issues 60 seconds in the past (avoid time jitter problem)
 EXPIRY=$((${NOW} + 600)) # Expires 10 minutes in the future
 
+# This function is used to apply Base64 encoding for the token to allow it to be passed on.
 b64enc() { openssl base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'; }
 
+# The JWT requires a Header, Payload and Signature as defined here.
 HEADER_JSON='{
     "typ":"JWT",
     "alg":"RS256"
@@ -111,7 +112,7 @@ PAYLOAD_JSON='{
     "exp":'"${EXPIRY}"',
     "iss":'"${BOT_APP_ID}"'
 }'
-# Payload encode
+# Payload encode in Base64
 PAYLOAD=$( echo -n "${PAYLOAD_JSON}" | b64enc )
 
 # Signature
@@ -138,23 +139,31 @@ printf '%s\n' "$GITHUB_TOKEN"
 
 function github_repository_dispatch()
 {
+# Because this script is intended to run asynchronously and may be called by a GitHub Workflow, we use
+# GitHub Repository Dispatch events to call back to the Workflow to allow it to continue.  This is a 
+# workaround to avoid two issues:
+# (1) Timeout of GitHub actions lasting over 6 hours.
+# (2) Billing costs associated with the GitHub hosted runner actively waiting whilst the backup runs.
+#
+# We supply 2 parameters to this function:
+#  EVENT_TYPE is a user-defined event to pass to the GitHub repository.   The backup worflow is triggered
+#  for either oracle-db-backup-sucess or oracle-db-backup-failure events.   These are the only 2 which
+#  should be used.
+#  JSON_PAYLOAD is the JSON originally passed to the script using the -j switch.  This allows the
+#  workflow to continue where it left off because this JSON contains the name of the environment, host
+#  and period of the backup, along with any associated parameters.
 EVENT_TYPE=$1
 JSON_PAYLOAD=$2
 GITHUB_TOKEN_VALUE=$(get_github_token | jq -r '.token')
+# We set the Phase in the JSON payload corresponding to whether the backup has succeeded or failed.
+# This is informational only - it is GitHub event type (oracle-db-backup-success/failure) which 
+# determines what the workflow does next.
 if [[ "$EVENT_TYPE" == "oracle-db-backup-success" ]]; then
     JSON_PAYLOAD=$(echo $JSON_PAYLOAD | jq -r '.Phase = "Backup Succeeded"')
 else
     JSON_PAYLOAD=$(echo $JSON_PAYLOAD | jq -r '.Phase = "Backup Failed"')
 fi
-info "Running Repository Dispatch:${EVENT_TYPE}:${JSON_PAYLOAD}:${GITHUB_TOKEN_VALUE}"
 JSON_DATA="{\"event_type\": \"${EVENT_TYPE}\",\"client_payload\":${JSON_PAYLOAD}}"
-info "JD1: $JSON_DATA"
-cat <<EOPOST
-curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token ${GITHUB_TOKEN_VALUE}"  --data ${JSON_DATA} ${REPOSITORY_DISPATCH}
-EOPOST
-cat <<EOCURL | tee -a /tmp/eocurl.txt
-curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token ${GITHUB_TOKEN_VALUE}"  --data ${JSON_DATA} ${REPOSITORY_DISPATCH}
-EOCURL
 curl -X POST -H "Accept: application/vnd.github+json" -H "Authorization: token ${GITHUB_TOKEN_VALUE}"  --data-raw "${JSON_DATA}" ${REPOSITORY_DISPATCH}
 RC=$?
 if [[ $RC -ne 0 ]]; then
@@ -185,9 +194,7 @@ update_ssm_parameter () {
   MESSAGE=$2
   info "Updating SSM Parameter ${SSM_PARAMETER} Message to ${MESSAGE}"
   SSM_VALUE=$(aws ssm get-parameter --name "${SSM_PARAMETER}" --query "Parameter.Value" --output text)
-  info "I: $NEW_SSM_VALUE"
   NEW_SSM_VALUE=$(echo ${SSM_VALUE} | jq -r --arg MESSAGE "$MESSAGE" '.Message=$MESSAGE')
-  info "B: $NEW_SSM_VALUE"
   if [[ "$STATUS" == "Success" ]]; then
      NEW_SSM_VALUE=$(echo ${NEW_SSM_VALUE} | jq -r '.Phase = "Backup Succeeded"')
   elif [[ "$STATUS" == "Running" ]]; then
@@ -195,7 +202,6 @@ update_ssm_parameter () {
   else
      NEW_SSM_VALUE=$(echo ${NEW_SSM_VALUE} | jq -r '.Phase = "Backup Failed"')
   fi
-  info "A: $NEW_SSM_VALUE"
   aws ssm put-parameter --name "${SSM_PARAMETER}" --type String --overwrite --value "${NEW_SSM_VALUE}" 1>&2
 }
 
@@ -751,7 +757,6 @@ if [[ ! -z "$JSON_INPUTS" ]]; then
    # dispatch event to indicate the end of the backup job run.  They do NOT
    # override the command line options passed to the script.
    JSON_INPUTS=$(echo $JSON_INPUTS | base64 --decode )
-   info "Original JSON Inputs to GitHub Action: $JSON_INPUTS"
 elif [[ ! -z "$REPOSITORY_DISPATCH" ]]; then
    error "JSON inputs must be supplied using the -j option if Repository Dispatch Events are requested."
 fi
