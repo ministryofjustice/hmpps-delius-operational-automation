d75305d66978095af5e5140cca0027fabeda0c72
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2024-04-18 19:39:27 +0100

    Why Not Work?

--- playbooks/oracle_backup/rman_backup.sh
+++ playbooks/oracle_backup/rman_backup.sh
@@ -166,7 +166,9 @@ fi
 # GitHub Actions only allows us to have 10 elements in the payload so we remove those which are
 # not necessary.  In this case we remove TargetHost since that is only relevant to the original
 # backup; any retries will use RmanTarget instead.
-JSON_PAYLOAD=$(echo $JSON_PAYLOAD | jq -r 'del(.client_payload.TargetHost)')
+info "PAYLOAD(1): $JSON_PAYLOAD"
+JSON_PAYLOAD=$(echo $JSON_PAYLOAD | jq -r 'del(.TargetHost)')
+info "PAYLOAD(2): $JSON_PAYLOAD"
 JSON_DATA="{\"event_type\": \"${EVENT_TYPE}\",\"client_payload\":${JSON_PAYLOAD}}"
 info "Posting repository dispatch event"
 cat <<EOCURL | tee -a /tmp/eocurl.txt
