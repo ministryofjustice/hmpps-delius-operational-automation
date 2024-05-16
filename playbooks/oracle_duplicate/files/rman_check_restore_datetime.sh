9424261528f7d24cb944f661fc7b40dc8e022b14
ranbeersingh1 <ranbeersinghtradedata@hotmail.com>
2024-05-16 09:12:19 +0100

    Use secrets instead of ssm parameter

--- playbooks/oracle_duplicate/files/rman_check_restore_datetime.sh
+++ playbooks/oracle_duplicate/files/rman_check_restore_datetime.sh
@@ -2,18 +2,48 @@
 . ~/.bash_profile
 SOURCE_DB=$1
 CATALOG_DB=$2
-RESTORE_DATETIME=${3:-NONE}
+CATALOG_SCHEMA=$3
+RESTORE_DATETIME=${4:-NONE}
 DATEFORMAT='YYMMDDHH24MISS'
 
-. /etc/environment
-SSMNAME="/${HMPPS_ENVIRONMENT}/${APPLICATION}/oracle-db-operation/rman/rman_password"
-RMANPASS=`aws ssm get-parameters --region ${REGION} --with-decryption --name ${SSMNAME} | jq -r '.Parameters[].Value'`
+# Determine the rman password depending where the catalog database resides
+if [[ ! ${CATALOG_DB} =~ ^\(DESCRIPTION.* ]]
+then
+  ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+  ROLE_ARN="arn:aws:iam::${ACCOUNT_ID}:role/${ASSUME_ROLE_NAME}"
+  SESSION="catalog-ansible"
+  SECRET_ACCOUNT_ID=$(aws ssm get-parameters --with-decryption --name account_ids | jq -r .Parameters[].Value |  jq -r 'with_entries(if (.key|test("hmpps-oem.*$")) then ( {key: .key, value: .value}) else empty end)' | jq -r 'to_entries|.[0].value' )
+  CREDS=$(aws sts assume-role --role-arn "${ROLE_ARN}" --role-session-name "${SESSION}"  --output text --query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]")
+  export AWS_ACCESS_KEY_ID=$(echo "${CREDS}" | tail -1 | cut -f1)
+  export AWS_SECRET_ACCESS_KEY=$(echo "${CREDS}" | tail -1 | cut -f2)
+  export AWS_SESSION_TOKEN=$(echo "${CREDS}" | tail -1 | cut -f3)
+  SECRET_ARN="arn:aws:secretsmanager:eu-west-2:${SECRET_ACCOUNT_ID}:secret:/oracle/database/${CATALOG_DB}/shared-passwords"
+  RMANUSER=${CATALOG_SCHEMA:-rcvcatowner}
+  RMANPASS=$(aws secretsmanager get-secret-value --secret-id "${SECRET_ARN}" --query SecretString --output text | jq -r .rcvcatowner)
+  unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
+else
+  INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+  APPLICATION=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=application" --query 'Tags[0].Value' --output text)
+  RMANUSER=rman19c
+  if [ "$APPLICATION" == "delius" ]
+  then
+    SECRET_ID="${ENVIRONMENT_NAME}-oracle-db-dba-passwords"
+  elif [ "$APPLICATION" == "delius-mis" ]
+  then
+    DATABASE_TYPE=$(aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=database" --query 'Tags[0].Value' --output text | cut -d'_' -f1)
+    SECRET_ID="${ENVIRONMENT_NAME}-oracle-${DATABASE_TYPE}-db-dba-passwords"
+    echo ${SECRET_ID}
+  fi
+  RMANPASS=$(aws secretsmanager get-secret-value --secret-id ${SECRET_ID} --query SecretString --output text | jq -r .rman)
+fi
+
 if [ -z ${RMANPASS} ]
 then
-  echo "Password for rman in aws parameter store ${SSMNAME} does not exist"
+  echo "Password for rman in secrets does not exist"
   exit 1
 fi
-CATALOG_CONNECT=rman19c/${RMANPASS}@${CATALOG_DB}
+
+CATALOG_CONNECT=${RMANUSER}/${RMANPASS}@${CATALOG_DB}
 
 if [ "${RESTORE_DATETIME}" != "NONE" ]
 then
