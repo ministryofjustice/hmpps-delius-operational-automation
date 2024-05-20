8844e6492dfcc92de4a55c2eb68b021af95ef5f6
ranbeersingh1 <ranbeersinghtradedata@hotmail.com>
2024-05-20 15:24:00 +0100

    Reposition statement

--- playbooks/oracle_backup/rman_backup.sh
+++ playbooks/oracle_backup/rman_backup.sh
@@ -583,6 +583,8 @@ EOF
     echo "  alter database open;"                                                                        >> $RMANCMDFILE
   elif [ "$BACKUP_TYPE" = "HOT" ]
   then
+    # Backups may fail when FRA is full, therefore backup archivelogs pior to archiving current log
+    echo "  backup archivelog all not backed up 1 times  $AL_TAG_FORMAT;"                                 >>$RMANCMDFILE
     if [[ "$ARCHIVELOGS" = "UNSPECIFIED" && $DATAFILES = "UNSPECIFIED" ]]    
     then
         if [ "$DB_STATUS" = "READ WRITE" ]
@@ -604,8 +606,6 @@ EOF
     if [[ "$ARCHIVELOGS" = "UNSPECIFIED" && $DATAFILES = "UNSPECIFIED" ]]    
     then
         echo "  database $DB_TAG_FORMAT;"                                                                     >>$RMANCMDFILE
-        # Backups may fail when FRA is full, therefore backup archivelogs pior to archiving current log
-        echo "  backup archivelog all not backed up 1 times  $AL_TAG_FORMAT;"                                 >>$RMANCMDFILE
         if [ "$DB_STATUS" = "READ WRITE" ]
         then echo "  alter system archive log current; "                                                      >>$RMANCMDFILE
         fi
