db2f033c8f4c97737ace7a293076c5ae8ba4672c
ranbeersingh1 <43067999+ranbeersingh1@users.noreply.github.com>
2024-10-03 13:03:53 +0100

    Replace datapump cli with database package

--- playbooks/oracle_duplicate/files/dfi_schemas.sh
+++ playbooks/oracle_duplicate/files/dfi_schemas.sh
@@ -2,60 +2,85 @@
  
  . ~/.bash_profile
 
+DFI_SCHEMAS="'DFIMIS_LANDING','DFIMIS_SUBSCRIBER','DFIMIS_DATA','DFIMIS_WORKING','DFIMIS_LOADER','DFIMIS_ABC'"
+DFI_SCHEMAS_NO=$(echo ${DFI_SCHEMAS} | sed 's/,/ /g;s/'\''//g' | wc -w | xargs)
 ACTION=${1}
 
 sqlplus -s / as sysdba << EOF
-
+SET SERVEROUTPUT ON
+SET ECHO OFF
+SET FEED OFF
+SET LINES 132
 DECLARE
 
-lc_count  INTEGER(1);
+lc_count          NUMBER;
+lc_dp_handle      NUMBER;
+lc_dump_name      VARCHAR2(100):='DFISCHEMAS';
+lc_job_state      VARCHAR2(100);
+lc_job_mode       VARCHAR2(100):=UPPER('${ACTION}');
+lc_job_name       VARCHAR2(100):=lc_job_mode||lc_dump_name;
+lc_dump_type      VARCHAR2(100):=DBMS_DATAPUMP.KU\$_FILE_TYPE_DUMP_FILE;
+lc_log_type       VARCHAR2(100):=DBMS_DATAPUMP.KU\$_FILE_TYPE_LOG_FILE;
 
 BEGIN
 
   SELECT COUNT(*)
   INTO lc_count
-  FROM dba_directories
-  WHERE directory_name = 'DATA_PUMP_DIR'
-  AND directory_path = '/u01/app/oracle/admin/${ORACLE_SID}/dpdump/';
+  FROM dba_users
+  WHERE username IN (${DFI_SCHEMAS});
 
-  IF lc_count = 0
+  IF lc_count = ${DFI_SCHEMAS_NO}
   THEN
-    EXECUTE IMMEDIATE q'[CREATE OR REPLACE DIRECTORY data_pump_dir AS '/u01/app/oracle/admin/${ORACLE_SID}/dpdump/']';
-  END IF;
-END;
-/
-EOF
 
+    SELECT COUNT(*)
+    INTO lc_count
+    FROM dba_directories
+    WHERE directory_name = 'DATA_PUMP_DIR'
+    AND directory_path = '/u01/app/oracle/admin/${ORACLE_SID}/dpdump/test';
 
-if [ "${ACTION}" == "export" ]
-then
-  expdp \"/ as sysdba\" schemas=dfimis_landing,dfimis_subscriber,dfimis_data,dfimis_working,dfimis_loader,dfimis_abc directory=data_pump_dir dumpfile=dfischemas.dmp logfile=expdfischemas.log job_name=expdfischemas reuse_dumpfiles=y
-elif [ "${ACTION}" == "import" ]
-then
+    IF lc_count = 0
+    THEN
+      EXECUTE IMMEDIATE q'[CREATE OR REPLACE DIRECTORY DATA_PUMP_DIR AS '/u01/app/oracle/admin/${ORACLE_SID}/dpdump/']';
+    END IF;
 
-  # To avoid ORA-31684: Object type ... already exists errors, drop objects prior import with the 
-  # exception of tables. Only squences have been identified so far, other object types maybe included
-  # in the future
-  sqlplus -s / as sysdba << EOF
+    lc_dp_handle := DBMS_DATAPUMP.open(operation => lc_job_mode, job_mode => 'SCHEMA', job_name => lc_job_name, version => 'LATEST');
 
-  BEGIN
+    IF lc_job_mode = 'EXPORT'
+    THEN
+       DBMS_DATAPUMP.add_file(handle => lc_dp_handle, filename => lc_dump_name||'.dmp', directory => 'DATA_PUMP_DIR',filetype => lc_dump_type, reusefile => 1);
+    ELSIF lc_job_mode = 'IMPORT'
+    THEN
 
-    FOR d IN ( SELECT  'DROP '||object_type||' '||owner||'.'||object_name cmd
-               FROM dba_objects 
-               WHERE owner IN (
-                'DFIMIS_LANDING',
-                'DFIMIS_SUBSCRIBER',
-                'DFIMIS_DATA',
-                'DFIMIS_WORKING',
-                'DFIMIS_LOADER',
-                'DFIMIS_ABC')
-               AND object_type IN ('SEQUENCE'))
+      -- To avoid ORA-31684: Object type ... already exists errors, drop objects prior import with the
+      -- exception of tables. Only squences have been identified so far, other object types maybe included
+      -- in the future
+      FOR d IN ( SELECT  'DROP '||object_type||' '||owner||'.'||object_name cmd
+                FROM dba_objects
+                WHERE owner IN (${DFI_SCHEMAS})
+                AND object_type IN ('SEQUENCE'))
       LOOP
         EXECUTE IMMEDIATE d.cmd;
       END LOOP;
-  END;
-  /
-EOF
 
-  impdp \"/ as sysdba\" schemas=dfimis_landing,dfimis_subscriber,dfimis_data,dfimis_working,dfimis_loader,dfimis_abc exclude=user directory=data_pump_dir dumpfile=dfischemas.dmp job_name=impdfischemas table_exists_action=replace logfile=impdfischemas.log
-fi
+      DBMS_DATAPUMP.metadata_filter(handle => lc_dp_handle, name => 'EXCLUDE_PATH_LIST', value => q'['USER']');
+      DBMS_DATAPUMP.set_parameter(lc_dp_handle, 'TABLE_EXISTS_ACTION','REPLACE');
+      DBMS_DATAPUMP.add_file(handle => lc_dp_handle, filename => lc_dump_name||'.dmp', directory => 'DATA_PUMP_DIR',filetype => lc_dump_type);
+
+    END IF;
+
+    DBMS_DATAPUMP.add_file(handle => lc_dp_handle, filename => lc_job_name||'.log', directory => 'DATA_PUMP_DIR', filetype => lc_log_type);
+    DBMS_DATAPUMP.metadata_filter(handle => lc_dp_handle, name => 'SCHEMA_LIST', value => q'[${DFI_SCHEMAS}]');
+    DBMS_DATAPUMP.start_job(lc_dp_handle);
+    DBMS_DATAPUMP.wait_for_job(handle => lc_dp_handle, job_state => lc_job_state );
+
+    DBMS_OUTPUT.PUT_LINE(lc_job_state);
+
+  ELSE
+
+    DBMS_OUTPUT.PUT_LINE('NODFISCHEMAS');
+
+  END IF;
+
+END;
+/
+EOF
