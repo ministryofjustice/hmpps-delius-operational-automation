fcea47e589740a3b95abc8ca3e3b87ea6e2c0e8e
bill-buchan <55987520+bill-buchan@users.noreply.github.com>
2024-10-14 14:57:33 +0100

    Dba 777 (#425)
    * Add Setup of Audit Schema
    
    * Add new workflow
    
    * Set target host
    
    * Fix directory name
    
    * Remove push event
    
    * Update for MP
    
    * Minor fixes
    
    * Wallet path no longer required

---
+++ playbooks/delius_audit_replication_schema/delius_audit_replication_schema/files/start_audit_checksumming.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#  Enable the Audited Interaction Data Checksumming Scheduler Job
+#
+
+. ~/.bash_profile
+
+sqlplus -L -S /nolog <<EOSQL
+connect / as sysdba
+SET PAGES 0
+SET FEEDBACK OFF
+SET ECHO OFF
+
+BEGIN
+   DBMS_SCHEDULER.enable('DELIUS_AUDIT_DMS_POOL.AUDIT_CHECKSUM_JOB');
+END;
+/
+
+EXIT
+EOSQL
