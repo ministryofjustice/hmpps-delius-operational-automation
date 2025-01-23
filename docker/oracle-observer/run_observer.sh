a78c8233e939171cfed371dafc55ee522dcc6283
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-01-23 15:48:32 +0000

    Stop previous observers
    Avoid build up of inactive observers in the configuration

--- docker/oracle-observer/run_observer.sh
+++ docker/oracle-observer/run_observer.sh
@@ -4,7 +4,10 @@
 OBSERVER_DIR=/u01/app/oracle/dg_observer
 mkdir ${OBSERVER_DIR}
 
+# We first stop any previous observers in the configuration which may have been left
+# inactive from a previous task run.
 # We start the observer in foreground mode; this keeps the docker container active
 dgmgrl /@${DATABASE_NAME} <<EOL
+stop observer all;
 start observer file is '${OBSERVER_DIR}/dg_broker.ora' logfile is '${OBSERVER_DIR}/dg_broker.log';
 EOL
