d22785c8a5b168abd74afdac128f0cc498155c59
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-01-24 12:01:19 +0000

    Add Standby DBs to wallet and wait for them to be available

--- docker/oracle-observer/create_wallet.sh
+++ docker/oracle-observer/create_wallet.sh
@@ -31,6 +31,14 @@ $ORACLE_HOME/bin/orapki wallet create -wallet "$WALLET_DIR" -auto_login_only
 # Add SYS password to the wallet for the target database
 echo "Adding SYS password to the Oracle wallet for the ${DATABASE_NAME} database..."
 $ORACLE_HOME/bin/mkstore -wrl ${WALLET_DIR} -createCredential ${DATABASE_NAME} sys ${SYS_PASSWORD}
+if [ "$STANDBYDB1_HOSTNAME" != "none" ]; then
+   echo "Adding SYS password to the Oracle wallet for the ${DATABASE_NAME}S1 database..."
+   $ORACLE_HOME/bin/mkstore -wrl ${WALLET_DIR} -createCredential ${DATABASE_NAME}S1 sys ${SYS_PASSWORD}
+fi
+if [ "$STANDBYDB2_HOSTNAME" != "none" ]; then
+   echo "Adding SYS password to the Oracle wallet for the ${DATABASE_NAME}S2 database..."
+   $ORACLE_HOME/bin/mkstore -wrl ${WALLET_DIR} -createCredential ${DATABASE_NAME}S2 sys ${SYS_PASSWORD}
+fi
 
 # Verify the wallet contents
 echo "Verifying wallet contents..."
--- docker/oracle-observer/run_observer.sh
+++ docker/oracle-observer/run_observer.sh
@@ -4,6 +4,46 @@
 OBSERVER_DIR=/u01/app/oracle/dg_observer
 mkdir ${OBSERVER_DIR}
 
+# Wait until all the databases in the configuration are running before performing any action.
+# This supports automatic stop/start of environments as we do not want the Observer to be
+# performing a failover during the morning startup.
+LOOP_COUNTER=0
+SQL_TEST="whenever oserror exit failure"
+TEST_CODE=1
+
+echo "Waiting for databases to become available..."
+while [[ ${LOOP_COUNTER} -le 600 && ${TEST_CODE} -gt 0 ]];
+do
+   echo "Sleeping 1 minute..."
+   sleep 60
+   echo ${SQL_TEST} | sqlplus -S -L /@${DATABASE_NAME} as sysdba
+   TEST_PRIMARY=$?
+   [[ $TEST_PRIMARY -eq 0 ]] && echo "Primary is available" || echo "Primary is unavailable"
+   if [ "$STANDBYDB1_HOSTNAME" != "none" ]; then
+      echo ${SQL_TEST} | sqlplus -S -L /@${DATABASE_NAME}S1 as sysdba
+      TEST_STANDBYDB1=$?
+      [[ $TEST_STANDBYDB1 -eq 0 ]] && echo "1st Standby is available" || echo "1st Standby is unavailable"
+   else
+      TEST_STANDBYDB1=0
+   fi
+   if [ "$STANDBYDB2_HOSTNAME" != "none" ]; then
+      echo ${SQL_TEST} | sqlplus -S -L /@${DATABASE_NAME}S2 as sysdba
+      TEST_STANDBYDB2=$?
+      [[ $TEST_STANDBYDB1 -eq 0 ]] && echo "2nd Standby is available" || echo "2nd Standby is unavailable"
+   else
+      TEST_STANDBYDB2=0
+   fi
+   TEST_CODE=$((TEST_PRIMARY+TEST_STANDBYDB1+TEST_STANDBYDB2))
+   LOOP_COUNTER=$((LOOP_COUNTER+60))
+done
+
+if [[ $TEST_CODE -gt 0 ]];
+then
+   echo "One or more databases in the configuration is unavailable.  Aborting..."
+   exit 1
+fi
+
+echo "Starting observer..."
 # We first stop any previous observers in the configuration which may have been left
 # inactive from a previous task run.
 # We start the observer in foreground mode; this keeps the docker container active
