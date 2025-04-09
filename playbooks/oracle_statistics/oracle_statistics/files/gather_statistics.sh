17c9eeda42dbf0af58b1a210daf655d2ec632d20
ranbeersingh1 <43067999+ranbeersingh1@users.noreply.github.com>
2025-04-09 16:08:30 +0100

    No need for phase

--- playbooks/oracle_statistics/oracle_statistics/files/gather_statistics.sh
+++ playbooks/oracle_statistics/oracle_statistics/files/gather_statistics.sh
@@ -75,14 +75,6 @@ function github_repository_dispatch()
 EVENT_TYPE=$1
 JSON_PAYLOAD=$2
 GITHUB_TOKEN_VALUE=$(get_github_token | jq -r '.token')
-# We set the Phase in the JSON payload corresponding to whether the stats has succeeded or failed.
-# This is informational only - it is GitHub event type (oracle-db-stats-success/failure) which 
-# determines what the workflow does next.
-if [[ "$EVENT_TYPE" == "oracle-db-statistics-success" ]]; then
-    JSON_PAYLOAD=$(echo $JSON_PAYLOAD | jq -r '.Phase = "Statistics Succeeded"')
-else
-    JSON_PAYLOAD=$(echo $JSON_PAYLOAD | jq -r '.Phase = "Statistics Failed"')
-fi
 # GitHub Actions only allows us to have 10 elements in the payload so we remove those which are
 # not necessary.  In this case we remove TargetHost since that is only relevant to the original
 # stats; any retries will use RmanTarget instead.
