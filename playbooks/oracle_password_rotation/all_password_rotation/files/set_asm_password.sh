abe176543a43e83e88cf9a285184532794ee521b
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2025-12-02 19:48:45 +0000

    Initial Version

--- playbooks/oracle_password_rotation/all_password_rotation/files/set_asm_password.sh
+++ playbooks/oracle_password_rotation/all_password_rotation/files/set_asm_password.sh
@@ -9,8 +9,7 @@ PATH=$PATH:/usr/local/bin
 
 # Check secret name if it belongs to OEM because the username for the sys password is different
 
-ASMSNMP_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${SECRET_NAME} --region eu-west-2 --query SecretString --output text| jq -r .${ASM_USERNAME})
-SYS_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${SECRET_NAME} --region eu-west-2 --query SecretString --output text| jq -r .${SYS_USERNAME})
+ASM_PASSWORD=$(aws secretsmanager get-secret-value --secret-id ${SECRET_NAME} --region eu-west-2 --query SecretString --output text| jq -r .${ASM_USERNAME})
 
 export ORACLE_SID=+ASM
 export ORAENV_ASK=NO
@@ -23,7 +22,6 @@ set pages 0
 set lines 30
 set echo on
 whenever sqlerror exit failure
-ALTER USER sys IDENTIFIED BY ${SYS_PASSWORD};
-ALTER USER asmsnmp IDENTIFIED BY ${ASMSNMP_PASSWORD};
+ALTER USER ${ASM_USERNAME} IDENTIFIED BY ${ASM_PASSWORD};
 exit;
 EOSQL
