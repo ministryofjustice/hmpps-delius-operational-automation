9567b34796f3e6872a787c954b75a3d3739bffce
Bill Buchan <bill.buchan@digital.justice.gov.uk>
2026-05-13 09:06:43 +0100

    Skip SET_DB_LINK_PASSWORD if it does not exist

--- playbooks/oracle_password_rotation/all_password_rotation/files/set_awsdms_dblink_password.sh
+++ playbooks/oracle_password_rotation/all_password_rotation/files/set_awsdms_dblink_password.sh
@@ -16,10 +16,30 @@ set pages 0
 set lines 30
 set echo on
 whenever sqlerror exit failure
+set serverout on
+
+/*
+    The procedure SET_DB_LINK_PASSWORD is not available prior to migration
+    from legacy to modernisation platform so use conditional compilation
+    to skip using it if it does not exist yet.
+*/
+
+COL proc_exists NEW_VALUE PROC_EXISTS_FLAG
+
+select 'proc_exists:'||CASE WHEN MAX(object_name) IS NULL THEN 'FALSE' ELSE 'TRUE' END proc_exists
+from dba_objects
+where object_name = 'SET_DB_LINK_PASSWORD';
+
+ALTER SESSION SET PLSQL_CCFLAGS='&PROC_EXISTS_FLAG';
+
 BEGIN
+   \$IF \$\$proc_exists \$THEN
    delius_audit_dms_pool.set_db_link_password(
      p_password=>'${DBLINK_PASSWORD}'
      );
+   \$ELSE
+   DBMS_OUTPUT.put_line('database link password setting procedure does not exist yet');
+   \$END
 END;
 /
 exit;
